1d89037935f57309f045d73fc828469ef194a1ad
Daniel Cohen <djc86@duke.edu>
2025-11-16 11:08:25 -0500

    Revert "feat: added TURN server"
    This reverts commit dad1d19b4303e5b746e2d1bee7e22ab68f4bc340.

--- jam/frontend.js
+++ jam/frontend.js
@@ -496,17 +496,7 @@
     }
 
     // WebRTC DataChannel P2P for chunk transfer
-    const rtcConfig = { 
-      iceServers: [
-        { urls: ['stun:stun.l.google.com:19302'] },
-        // Free TURN servers (use for testing only, get your own for production)
-        { urls: 'turn:openrelay.metered.ca:80', username: 'openrelayproject', credential: 'openrelayproject' },
-        { urls: 'turn:openrelay.metered.ca:443', username: 'openrelayproject', credential: 'openrelayproject' },
-        { urls: 'turn:openrelay.metered.ca:443?transport=tcp', username: 'openrelayproject', credential: 'openrelayproject' }
-      ],
-      // Force relay for restrictive networks (university WiFi):
-      iceTransportPolicy: 'relay'
-    };
+    const rtcConfig = { iceServers: [{ urls: ['stun:stun.l.google.com:19302'] }] };
     // Per-peer connections/channels
     const pcByPeer = new Map();
     const dcByPeer = new Map();
